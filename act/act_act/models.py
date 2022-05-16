e2c0763972cc94d7cfb1eecd45b9adfc8719cc48
Kim Tae Seong <dnfrkwhr2424@naver.com>
2022-05-16 14:57:53 +0900

    make a model

--- act/act_act/models.py
+++ act/act_act/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Works(models.Model):
+
+    date = models.DateField(auto_now_add=True)
+    work = models.CharField(max_length=10)
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date']
